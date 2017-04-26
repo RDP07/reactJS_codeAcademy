c13cd14fcfc5811711e272035e33598aa3ecd755
RDP <rdupaw@gmail.com>
2017-04-25 22:10:34 -0500

    Edit app.js for JSX event listener challange

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var goose = 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-goose.jpg';
 
-var gooseImg = (
-	<img src={goose} alt="goose" />
+function makeDoggy (e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+var kitty = (
+	<img 
+		src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg" 
+		alt="kitty" />
 );
 
-ReactDOM.render(gooseImg, document.getElementById('app'));
+ReactDOM.render(null, null);
