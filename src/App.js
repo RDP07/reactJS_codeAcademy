cce2302bb0a3bfc7a2ed1911241321a587092cf4
RDP <rdupaw@gmail.com>
2017-04-25 22:02:29 -0500

    Wrap gooseImg value in ()

--- src/App.js
+++ src/App.js
@@ -3,4 +3,6 @@ var ReactDOM = require('react-dom');
 
 var goose = 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-goose.jpg';
 
-var gooseImg = <img src={goose} />
+var gooseImg = (
+	<img src={goose} />
+);
