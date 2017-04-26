ad28355a188813cc225d0d303f5556cff8bc7941
RDP <rdupaw@gmail.com>
2017-04-25 22:18:33 -0500

    Edit app.js for if statements challanges

--- src/App.js
+++ src/App.js
@@ -2,17 +2,12 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 
-function makeDoggy (e) {
-  // Call this extremely useful function on an <img>.
-  // The <img> will become a picture of a doggy.
-  e.target.setAttribute('src', 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg');
-  e.target.setAttribute('alt', 'doggy');
+function coinToss () {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
 }
 
-var kitty = (
-	<img onClick={makeDoggy}
-		src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg" 
-		alt="kitty" />
-);
-
-ReactDOM.render(kitty, document.getElementById('app'));
+var pics = {
+  kitty: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg'
+};
