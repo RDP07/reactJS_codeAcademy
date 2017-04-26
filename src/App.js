630e417b7a0e6d920927e55df472d493d7d80a4d
RDP <rdupaw@gmail.com>
2017-04-25 21:33:12 -0500

    Fix react and edit app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-// Write code here:
-var myList = (
-  <ul>
-    <li>Angular</li>
-    <li>React</li>
-    <li>JavaScript</li>
-    <li>Developing</li>
-    <li>HTML</li>
-    <li>CSS</li>
-  </ul>
+var myDiv = (
+	<div className='big'>
+	  I AM A BIG DIV
+	</div>
 );
 
 ReactDOM.render(
-myList,
-document.getElementById('container')
+myDiv,
+document.getElementById('app')
 );
