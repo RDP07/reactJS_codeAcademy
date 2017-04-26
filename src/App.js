a1aee4103ce8dd8dfd0db6f47f4391b84cf49c3c
RDP <rdupaw@gmail.com>
2017-04-25 21:40:08 -0500

    Add 2 + 3 in h1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var myDiv = (
-	<div className='big'>
-	  I AM A BIG DIV
-	</div>
-);
-
 ReactDOM.render(
-myDiv,
-document.getElementById('app')
+	<h1>2 + 3</h1>,
+	document.getElementById('app')
 );
