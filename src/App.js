10dc924c41aff06102f66e8552905393a3468396
RDP <rdupaw@gmail.com>
2017-04-25 22:07:18 -0500

    Add ReactDOM.render for gooseImg and add alt

--- src/App.js
+++ src/App.js
@@ -4,5 +4,7 @@ var ReactDOM = require('react-dom');
 var goose = 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-goose.jpg';
 
 var gooseImg = (
-	<img src={goose} />
+	<img src={goose} alt="goose" />
 );
+
+ReactDOM.render(gooseImg, document.getElementById('app'));
