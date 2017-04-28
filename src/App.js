c140a919b9dff7d56cbe0a069ed1dac5d0547223
RDP <rdupaw@gmail.com>
2017-04-27 19:37:12 -0500

    Render greatestDivEver with React.createElement solution

--- src/App.js
+++ src/App.js
@@ -3,3 +3,13 @@ var ReactDOM = require('react-dom');
 
 /*var greatestDivEver = <div>i am div</div>;*/
 
+var greatestDivEver = React.createElement(
+"div",
+null,
+'i am div'
+);
+
+ReactDOM.render(
+	greatestDivEver,
+	document.getElementById('app')
+	);
