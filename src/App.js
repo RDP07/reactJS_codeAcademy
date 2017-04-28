6a958e5495b027b611a35dc98339f80ff108374f
RDP <rdupaw@gmail.com>
2017-04-27 19:30:46 -0500

    Edit return to ass unique key to each li item

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ var ReactDOM = require('react-dom');
 var people = ['Rowe', 'Prevost', 'Gare'];
 
 var peopleLIs = people.map(function(person){
-  return <li>{person}</li>;
+  return <li key={'person_' + i} >{person}</li>;
 });
 
 ReactDOM.render(
