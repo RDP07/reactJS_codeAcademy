a2e128f6cb4b182ee9d65d6476835357d6dbd1f6
RDP <rdupaw@gmail.com>
2017-04-27 19:01:19 -0500

    Solution with react listing out array in a <ul>

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ var ReactDOM = require('react-dom');
 var people = ['Rowe', 'Prevost', 'Gare'];
 
 var peopleLIs = people.map(function(person){
-  // return statement goes here:
-
+  return <li>{person}</li>
 });
 
-// ReactDOM.render goes here:
+ReactDOM.render(
+<ul>{peopleLIs}</ul>,
+document.getElementById('app')
+);
