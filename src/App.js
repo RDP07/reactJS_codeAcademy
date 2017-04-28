6bd79c3f0fdb273d814192cfb85f7d15a7e8f5ba
RDP <rdupaw@gmail.com>
2017-04-27 19:36:26 -0500

    Render greatestDivEver with React.createElement challange

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var React = require('react');
-var ReactDOM = require('react-dom');
+/*var greatestDivEver = <div>i am div</div>;*/
 
-var people = ['Rowe', 'Prevost', 'Gare'];
-
-var peopleLIs = people.map(function(person){
-  return <li key={'person_' + i} >{person}</li>;
-});
-
-ReactDOM.render(
-<ul>{peopleLIs}</ul>,
-document.getElementById('app')
-);
