2c044b242268330b4e32cc159f5fb0bb6423c6be
RDP <rdupaw@gmail.com>
2017-04-27 19:04:49 -0500

    Setup for JSX keys challange

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var React = require('react');
+var ReactDOM = require('react-dom');
 
 var people = ['Rowe', 'Prevost', 'Gare'];
 
 var peopleLIs = people.map(function(person){
-  return <li>{person}</li>
+  return <li>{person}</li>;
 });
 
 ReactDOM.render(
