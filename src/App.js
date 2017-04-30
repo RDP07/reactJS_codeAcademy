3d0c33a4cd8c3a8c78ef909adc14e5bf169dae36
RDP <rdupaw@gmail.com>
2017-04-30 15:35:42 -0500

    Create Quote class and render to DOM by ID app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Quote = React.createClass({
+  render: function() {
+  	return (
+  	  <blockquote>
+  		<p>
+  		  What is important now is to recover our senses.
+  		</p>
+  		<cite>
+  		  <a target="_blank"
+  		    href="http://bit.ly/1LvSLUA">
+  		    Sustan Sontag
+  		  </a>
+  		</cite>
+  	  </blockquote>
+  	);
+  }
+});
+
+ReactDOM.render(
+  <Quote />,
+  document.getElementById('app')
+);
