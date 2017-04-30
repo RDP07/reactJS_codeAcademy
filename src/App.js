d4dad750bbacb9b76a871dd55623ca9163f55099
RDP <rdupaw@gmail.com>
2017-04-30 16:27:47 -0500

    Set up app.js for logic in a render challange

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var owl = {
-  title: "Excellent Owl",
-  src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-owl.jpg"
-};
-
-var Owl = React.createClass({
-	render: function() {
-	  return (
-	  	<div>
-	  	  <h1> { owl.title } </h1>
-	  	  <img 
-	  	    src= { owl.src }
-	  	    alt= { owl.title } />
-	  	</div>
-	  );
-	}
-});
-
-ReactDOM.render(
-  <Owl />,
-  document.getElementById('app')
-  );
+var friends = [
+  {
+    title: "Yummmmmmm",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-monkeyweirdo.jpg"
+  },
+  {
+    title: "Hey Guys!  Wait Up!",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-earnestfrog.jpg"
+  },
+  {
+    title: "Yikes",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-alpaca.jpg"
+  }
+];
